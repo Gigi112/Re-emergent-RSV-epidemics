dcb3362de96fe4892b716d0826d59ca8dd683e33
Zhe Zheng (Gigi) <709141475@qq.com>
2021-12-01 15:17:58 -0500

    Update externalforce.R

--- externalforce.R
+++ externalforce.R
@@ -1,6 +1,6 @@
 I_ex_model <- function(t,y,parms, time.step='month'){
   
-  States<-array(y, dim=dim(parms$yinit.matrix))
+  States<-array(y, dim=dim(parms$yinit.matrix))  # MSIS states and the number of age groups
   dimnames(States) <- dimnames(parms$yinit.matrix)
   
   if(parms$time.step=='month'){
@@ -21,42 +21,43 @@ I_ex_model <- function(t,y,parms, time.step='month'){
     ##  I_ex=1/100000*St ; I_ex=5/100000*St ; I_ex=10/100000*St and etc
   }
   
-  omega = 1/(parms$DurationMatImmunityDays/length.step)
+  omega = 1/(parms$DurationMatImmunityDays/length.step) # rate of waning of maternal immunity
   
   mu= 1/parms$WidthAgeClassMonth
   if(parms$time.step=='week'){
-    mu= 1/(WidthAgeClassMonth*4.345)
+    mu= 1/(WidthAgeClassMonth*4.345) # aging process
   }
   
-  gamma1= 1/(parms$dur.days1/length.step)  #converts 1/days to 1/lenth.step
-  gamma2= 1/(parms$dur.days2/length.step)  
-  gamma3= 1/(parms$dur.days3/length.step)  
-  gamma4= gamma3  #??Is this right?? Yes, gamma3 stands for rate of recovery from subsequent infection
+  gamma1= 1/(parms$dur.days1/length.step)  #converts 1/days to 1/lenth.step # gamma1 stands for rate of recovery from the first infection
+  gamma2= 1/(parms$dur.days2/length.step)  # gamma2 stands for rate of recovery from the second infection
+  gamma3= 1/(parms$dur.days3/length.step)  # gamma3 stands for rate of recovery from subsequent infection
+  gamma4= gamma3  # gamma3 stands for rate of recovery from subsequent infection
   
-  #Pull out the states  for the model as vectors
-  M <-  States[,'M']
-  S0 <-  States[,'S0']
-  I1 <-  States[,'I1']
+ #Pull out the states  for the model as vectors
+  M <-  States[,'M'] # protected by maternal immunity
+  S0 <-  States[,'S0'] # fully suceptible to infections
+  I1 <-  States[,'I1'] # first time being infected
   
-  S1 <-  States[,'S1']
-  I2 <-  States[,'I2']
+  S1 <-  States[,'S1'] # partial immunity after first infection
+  I2 <-  States[,'I2'] # second time infections
   
-  S2 <-  States[,'S2']
-  I3 <-  States[,'I3']
+  S2 <-  States[,'S2'] # further boosted immunity after second time infections
+  I3 <-  States[,'I3'] # third time infections
   
-  S3 <-  States[,'S3']
-  I4 <-  States[,'I4']
+  S3 <-  States[,'S3'] # further boosted immunity after subsequent infections
+  I4 <-  States[,'I4'] # any subsequent infections
   
-  N.ages <- length(M)
+  N.ages <- length(M)# age groups 
   
   ###Check the standardization of beta and overall structure of lambda here
   ##force of transmission##################
   seasonal.txn <- (1+b1*cos(2*pi*(t-phi*period)/period))# seasonality waves
-  b <- parms$baseline.txn.rate[t]/ (parms$dur.days1/length.step) # transmission probability per unit time
+  b <- parms$baseline.txn.rate[t]/ (parms$dur.days1/length.step) # transmission probability per unit time; baseline.txn.rate is a vector stands for the transmissibility at that time point
+  # will be lower during COVID mitigation period
   beta <-  (b/100)/(sum(yinit.matrix)^(1-q))*c2# q depends on transmission type (whether depends on population density or not); c2 is the contact matrix
   
   beta_a_i <- seasonal.txn * beta/sum(States)
-  infectiousN <- I1 + rho1*I2 + rho2*I3 + rho2*I4 + rho2*I_ex # include internal force and external force
+  infectiousN <- I1 + rho1*I2 + rho2*I3 + rho2*I4 + rho2*I_ex # include internal force and external force # infectious population times their relative infectiousness
   
   lambda <- infectiousN %*% beta_a_i 
   lambda <- as.vector(lambda)
@@ -71,28 +72,28 @@ I_ex_model <- function(t,y,parms, time.step='month'){
   #mu represents aging to the next class
   #um is death rate
   
-  Aging.Prop <- c(0,mu[1:(N.ages-1)])
+  Aging.Prop <- c(0,mu[1:(N.ages-1)]) # aging in
   
-  dy[,'M'] <- period.birth.rate*sum(States) - 
-    (omega+(mu+um))*M +
-    Aging.Prop*c(0,M[1:(N.ages-1)]) 
+  dy[,'M'] <- period.birth.rate*sum(States) - # birth into protected status
+    (omega+(mu+um))*M + # waning immunity + aging out + death/immigration/emmigration (um can be either positive or negative based on the the population growth)
+    Aging.Prop*c(0,M[1:(N.ages-1)]) # aging in
   
-  dy[,'S0'] <- omega*M -
-    lambda*S0 -
-    (mu + um)*S0 + 
-    Aging.Prop*c(0,S0[1:(N.ages-1)]) 
+  dy[,'S0'] <- omega*M - # after the maternal immunity wanes, individuals are susceptible to infections
+    lambda*S0 - # getting infections
+    (mu + um)*S0 + # aging out + death/immigration/emmigration (um can be either positive or negative based on the the population growth)
+    Aging.Prop*c(0,S0[1:(N.ages-1)]) # aging in
   
-  dy[,'I1'] <-   lambda*S0 - 
-    (gamma1 + mu + um)*I1 + 
-    Aging.Prop*c(0,I1[1:(N.ages-1)]) 
+  dy[,'I1'] <-   lambda*S0 - # being infected
+    (gamma1 + mu + um)*I1 + # rate of recovery + aging out + death/immigration/emmigration (um can be either positive or negative based on the the population growth)
+    Aging.Prop*c(0,I1[1:(N.ages-1)]) # aging in
   
-  dy[,'S1'] <- gamma1*I1 - 
-    sigma1*lambda*S1 - 
+  dy[,'S1'] <- gamma1*I1 - # after recovered from infections, individuals gain partial immunity
+    sigma1*lambda*S1 - # which reduced their risk of re-infection; sigma 1 here represnts the reduced risk 
     (mu+um)*S1 + 
     Aging.Prop*c(0,S1[1:(N.ages-1)]) 
   
   dy[,'I2'] <- sigma1*lambda*S1 - 
-    gamma2*I2-(mu + um)*I2 + 
+    gamma2*I2-(mu + um)*I2 + # second time infections will last shorter than the first time infection
     Aging.Prop*c(0,I2[1:(N.ages-1)]) 
   
   dy[,'S2'] <- gamma2*I2 - 
